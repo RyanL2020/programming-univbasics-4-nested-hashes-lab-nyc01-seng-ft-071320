a938af514ce3ddc1e48c583504cdc1452ac56743
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-22 18:06:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -7,14 +7,22 @@ def hopper
 end
 
 def alan_kay_is_known_for
-
-	programmer_hash ={
-      :alan_kay => {
-        :known_for => "Object Orientation",
-      }
+	programmer_hash =
+		{
+			:grace_hopper => {
+				:known_for => "COBOL",
+				:languages => ["COBOL", "FORTRAN"]
+			},
+			:alan_kay => {
+				:known_for => "Object Orientation",
+				:languages => ["Smalltalk", "LISP"]
+			},
+			:dennis_ritchie => {
+				:known_for => "Unix",
+				:languages => ["C"]
+			}
 		}
-		end
-		programmer_hash[:alan_kay][:known_for]
+
 	end
 
 def dennis_ritchies_language
