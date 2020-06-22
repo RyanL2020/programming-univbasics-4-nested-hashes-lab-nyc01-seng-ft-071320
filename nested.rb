8259a21c8e735804745bbad169f5330741767f1c
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-22 18:26:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
@@ -93,7 +93,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
